923cfa58cedf6970f42750df6fcd6b079a8bb4b0
XeVolo <xevolo12@gmail.com>
2024-04-22 18:10:16 +0200

    changed models (auth_user)

--- budget/admin.py
+++ budget/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-
-from .models import Wallet,Expense,Revenue,Category,User
+from .models import Wallet, Expense, Revenue, Category
 
 admin.site.register(Wallet)
 admin.site.register(Expense)
 admin.site.register(Revenue)
 admin.site.register(Category)
-admin.site.register(User)
--- budget/models.py
+++ budget/models.py
@@ -1,18 +1,13 @@
 from django.db import models
-
-from django.db import models
-
-class User(models.Model):
-    name = models.CharField(max_length=100)
-    surname = models.CharField(max_length=100)
-    phone_number = models.IntegerField()
-    email = models.EmailField()
-    password = models.CharField(max_length=100)
+from django.contrib.auth.models import User
 
 class Wallet(models.Model):
-    name = models.CharField(max_length=100)
+    users = models.ManyToManyField(User, related_name='wallets')
     balance = models.DecimalField(max_digits=10, decimal_places=2)
-    description = models.CharField(max_length=255, blank=True, null=True)
+    description = models.CharField(max_length=255)
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
 
 class Expense(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -20,8 +15,8 @@ class Expense(models.Model):
     title = models.CharField(max_length=255)
     operation_date = models.DateField()
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    category = models.ForeignKey('Category', on_delete=models.CASCADE)
-    description = models.CharField(max_length=255, blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.TextField(blank=True)
 
 class Revenue(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -29,10 +24,5 @@ class Revenue(models.Model):
     title = models.CharField(max_length=255)
     operation_date = models.DateField()
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    category = models.ForeignKey('Category', on_delete=models.CASCADE)
-    description = models.CharField(max_length=255, blank=True, null=True)
-
-class Category(models.Model):
-    name = models.CharField(max_length=100)
-
-
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.TextField(blank=True)
