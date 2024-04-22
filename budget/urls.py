028c448e626ccfa7e8586ccde96464b3f3ff67eb
XeVolo <xevolo12@gmail.com>
2024-04-22 18:51:07 +0200

    views

--- budget/urls.py
+++ budget/urls.py
@@ -1,7 +1,33 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    # Widoki dla modelu Wallet
+    path('wallet/', views.WalletListView.as_view(), name='wallet_list'),
+    path('wallet/create/', views.WalletCreateView.as_view(), name='wallet_create'),
+    path('wallet/<int:pk>/update/', views.WalletUpdateView.as_view(), name='wallet_update'),
+    path('wallet/<int:pk>/delete/', views.WalletDeleteView.as_view(), name='wallet_delete'),
+
+    # Widoki dla modelu Expenses
+    path('expenses/', views.ExpenseListView.as_view(), name='expenses_list'),
+    path('expenses/create/', views.ExpenseCreateView.as_view(), name='expenses_create'),
+    path('expenses/<int:pk>/update/', views.ExpenseUpdateView.as_view(), name='expenses_update'),
+    path('expenses/<int:pk>/delete/', views.ExpenseDeleteView.as_view(), name='expenses_delete'),
+
+    # Widoki dla modelu Revenues
+    path('revenues/', views.RevenueListView.as_view(), name='revenues_list'),
+    path('revenues/create/', views.RevenueCreateView.as_view(), name='revenues_create'),
+    path('revenues/<int:pk>/update/', views.RevenueUpdateView.as_view(), name='revenues_update'),
+    path('revenues/<int:pk>/delete/', views.RevenueDeleteView.as_view(), name='revenues_delete'),
+
+    # Widoki dla modelu Category
+    path('category/', views.CategoryListView.as_view(), name='category_list'),
+    path('category/create/', views.CategoryCreateView.as_view(), name='category_create'),
+    path('category/<int:pk>/update/', views.CategoryUpdateView.as_view(), name='category_update'),
+    path('category/<int:pk>/delete/', views.CategoryDeleteView.as_view(), name='category_delete'),
+
+    # Widoki rejestracji i logowania
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
 ]
--- budget/views.py
+++ budget/views.py
@@ -1,5 +1,120 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from .models import Wallet, Expense, Revenue, Category
+from django.urls import reverse_lazy
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # Przekierowanie po rejestracji
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')  # Przekierowanie po zalogowaniu
+    else:
+        form = AuthenticationForm()
+    return render(request, 'registration/login.html', {'form': form})
+
+def user_logout(request):
+    logout(request)
+    return redirect('home')  # Przekierowanie po wylogowaniu
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+class WalletListView(ListView):
+    model = Wallet
+    template_name = 'wallet_list.html'
+
+class WalletCreateView(CreateView):
+    model = Wallet
+    fields = ['balance', 'description']
+    success_url = reverse_lazy('wallet_list')
+    template_name = 'wallet_form.html'
+
+class WalletUpdateView(UpdateView):
+    model = Wallet
+    fields = ['balance', 'description']
+    success_url = reverse_lazy('wallet_list')
+    template_name = 'wallet_form.html'
+
+class WalletDeleteView(DeleteView):
+    model = Wallet
+    success_url = reverse_lazy('wallet_list')
+    template_name = 'wallet_confirm_delete.html'
+
+
+class ExpenseListView(ListView):
+    model = Expense
+    template_name = 'expense_list.html'
+
+class ExpenseCreateView(CreateView):
+    model = Expense
+    fields = ['user', 'wallet', 'title', 'operation_date', 'amount', 'category', 'description']
+    success_url = reverse_lazy('expenses_list')
+    template_name = 'expense_form.html'
+
+class ExpenseUpdateView(UpdateView):
+    model = Expense
+    fields = ['user', 'wallet', 'title', 'operation_date', 'amount', 'category', 'description']
+    success_url = reverse_lazy('expense_list')
+    template_name = 'expense_form.html'
+
+class ExpenseDeleteView(DeleteView):
+    model = Expense
+    success_url = reverse_lazy('expense_list')
+    template_name = 'expense_confirm_delete.html'
+
+
+class RevenueListView(ListView):
+    model = Revenue
+    template_name = 'revenue_list.html'
+
+class RevenueCreateView(CreateView):
+    model = Revenue
+    fields = ['user', 'wallet', 'title', 'operation_date', 'amount', 'category', 'description']
+    success_url = reverse_lazy('revenue_list')
+    template_name = 'revenue_form.html'
+
+class RevenueUpdateView(UpdateView):
+    model = Revenue
+    fields = ['user', 'wallet', 'title', 'operation_date', 'amount', 'category', 'description']
+    success_url = reverse_lazy('revenues_list')
+    template_name = 'revenue_form.html'
+
+class RevenueDeleteView(DeleteView):
+    model = Revenue
+    success_url = reverse_lazy('revenue_list')
+    template_name = 'revenue_confirm_delete.html'
+
+class CategoryListView(ListView):
+    model = Category
+    template_name = 'category_list.html'
+
+class CategoryCreateView(CreateView):
+    model = Category
+    fields = ['name']
+    success_url = reverse_lazy('category_list')
+    template_name = 'category_form.html'
+
+class CategoryUpdateView(UpdateView):
+    model = Category
+    fields = ['name']
+    success_url = reverse_lazy('category_list')
+    template_name = 'category_form.html'
+
+class CategoryDeleteView(DeleteView):
+    model = Category
+    success_url = reverse_lazy('category_list')
+    template_name = 'category_confirm_delete.html'
