8931cb35265185393553290391471de49cb4ed51
Mixus <mateuszokragly@gmail.com>
2024-05-14 22:35:40 +0200

    Logowanie

--- budget/urls.py
+++ budget/urls.py
@@ -4,8 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.welcome, name='welcome'),
     path('home/', views.home, name='home'),
+
     # Widoki dla modelu Wallet
     path('wallet/', views.WalletListView.as_view(), name='wallet_list'),
+    path('wallet/<int:pk>/', views.WalletDetailView.as_view(), name='wallet_details'),
     path('wallet/create/', views.WalletCreateView.as_view(), name='wallet_create'),
     path('wallet/<int:pk>/update/', views.WalletUpdateView.as_view(), name='wallet_update'),
     path('wallet/<int:pk>/delete/', views.WalletDeleteView.as_view(), name='wallet_delete'),
