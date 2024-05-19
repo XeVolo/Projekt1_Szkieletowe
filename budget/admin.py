96b5a23b4ed07eb2ff786186bd663898abab42bf
XeVolo <xevolo12@gmail.com>
2024-05-19 04:10:56 +0200

    admin

--- budget/admin.py
+++ budget/admin.py
@@ -1,7 +1,37 @@
 from django.contrib import admin
-from .models import Wallet, Expense, Revenue, Category
+from .models import Wallet, Category, Expense, Revenue
 
-admin.site.register(Wallet)
-admin.site.register(Expense)
-admin.site.register(Revenue)
-admin.site.register(Category)
+
+# Define custom admin classes for each model
+
+
+class WalletAdmin(admin.ModelAdmin):
+    list_display = ('description', 'balance')
+    filter_horizontal = ('users',)
+
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'wallet', 'operation_date', 'amount', 'category')
+    list_filter = ('user', 'wallet', 'category')
+    search_fields = ('title', 'description')
+
+
+
+class RevenueAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'wallet', 'operation_date', 'amount', 'category')
+    list_filter = ('user', 'wallet', 'category')
+    search_fields = ('title', 'description')
+
+
+# Register the custom admin classes with the admin site
+
+admin.site.register(Wallet, WalletAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Expense, ExpenseAdmin)
+admin.site.register(Revenue, RevenueAdmin)
